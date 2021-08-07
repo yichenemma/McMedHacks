ebf91a3885b04dcda75721ed2dac3cf367869142
Yichen Wu <yichen.wu3@mail.mcgill.ca>
2021-08-07 13:31:31 -0400

    file upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import datetime
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.H2('Upload the CT Lung scan'),
+    html.Div('In the format of either jpg, jpeg, png'),
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=False
+    ),
+    html.Div(id='output-image-upload'),
+])
+
+
+def parse_contents(contents, filename, date):
+    return html.Div([
+        html.H5(filename),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.Img(src=contents,height=300,width=200)
+
+    ])
+
+
+@app.callback(Output('output-image-upload', 'children'),
+              Input('upload-image', 'contents'),
+              State('upload-image', 'filename'),
+              State('upload-image', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = parse_contents(list_of_contents, list_of_names, list_of_dates)
+        return children
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
